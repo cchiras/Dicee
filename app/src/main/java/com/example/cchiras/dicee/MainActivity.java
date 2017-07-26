a6b63f91d352eb771d29eaa1af3037076e473a9e
chris <cchiras@xm.com>
2017-07-26 17:47:15 +0300

    dicee 60%

--- app/src/main/java/com/example/cchiras/dicee/MainActivity.java
+++ app/src/main/java/com/example/cchiras/dicee/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.cchiras.dicee;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button rollButton;
+        rollButton = (Button) findViewById(R.id.rollButton);
+        ImageView leftDice = (ImageView) findViewById(R.id.image_leftDice);
+        ImageView rightDice = (ImageView)findViewById(R.id.image_rightDice);
+
+        rollButton.setOnClickListener(v {
+            @Override
+            public void onClick(View view) {
+
+                Log.d("Dicee", "The button has been pressed");
+            }
+        });
+
+
+
     }
 }
